eb9fd55565b561eb790a784564126e2cfbcc8801
Bedrich Pisl <bedapisl@gmail.com>
2023-08-31 18:10:50 +0200

    Git config, scripts folder

--- install.sh
+++ install.sh
@@ -8,6 +8,14 @@ ln -s ./dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 ln -s ./dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
 
 
-ln -s ./scripts/restart_network_manager.sh ~/restart_network_manager.sh
-ln -s ./scripts/brightness.sh ~/brightness.sh
+mkdir ~/scripts
+
+ln -s ./scripts/restart_network_manager.sh ~/scripts/restart_network_manager.sh
+ln -s ./scripts/brightness.sh ~/scripts/brightness.sh
+
+read -p "Git email: " git_email
+
+git config --global user.email git_email
+git config --global user.name "Bedrich Pisl"
+git config --global core.editor "vim"
 
